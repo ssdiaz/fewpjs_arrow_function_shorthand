9573a1d8012a5590e85ed8ca0b2cbdda4a5424bd
Samantha Diaz <samidiaz3@gmail.com>
2022-01-30 23:39:48 -0500

    commit tree d6c7e066986e75083c4f608fbccd4fe83e6a12b5

---
+++ index.js
@@ -0,0 +1,16 @@
+// 1) has a functional expression called divide
+// 2) divide divides 2000 by 100
+function divide(){
+    return 2000 / 100
+}
+    // solution
+        //  let divide = () => 2000/100
+
+// 3) has an arrow function called square
+// 4) square arrow function multiplies a number times itself
+let square = x => x * x
+
+
+// 5) has an arrow function called add
+// 6) add arrow function takes two parameters and adds them together
+let add = (a, b) => a + b
